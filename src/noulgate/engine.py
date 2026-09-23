561e4ffb2b6013a7f7cacf220ade8c354899e5b5
Manish Anchan <manishanchan789@gmail.com>
2026-09-23 20:04:45 +0530

    chore: remove redundant engine.py and proxy.py backwards-compatibility shims

--- src/noulgate/engine.py
+++
@@ -1,9 +0,0 @@
-"""
-noulgate.engine
-===============
-Backwards-compatibility module forwarding to noulgate.core.
-"""
-
-from noulgate.core import NoulGateEngine, PruneResult, ToolDefinition
-
-__all__ = ["NoulGateEngine", "ToolDefinition", "PruneResult"]
